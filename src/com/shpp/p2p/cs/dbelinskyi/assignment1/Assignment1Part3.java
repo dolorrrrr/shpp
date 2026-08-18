7faef4b562a8171151677932b06113aa763b4c58
d.belinskyi <belinskijplay@gmail.com>
2026-08-18 17:01:20 +0300

    Кінець робочого дня

--- src/com/shpp/p2p/cs/dbelinskyi/assignment1/Assignment1Part3.java
+++ src/com/shpp/p2p/cs/dbelinskyi/assignment1/Assignment1Part3.java
@@ -2,8 +2,7 @@ package com.shpp.p2p.cs.dbelinskyi.assignment1;
 
 import com.shpp.p2p.cs.dbelinskyi.SuperKarel;
 
-public class Assignment1Part3 extends SuperKarel
-{
+public class Assignment1Part3 extends SuperKarel {
     /**
      * Задача 3 - Шахова дошка
      * ПРЯМОКУТНИК - ЦЕ НЕ ТІЛЬКИ КВАДРАТ
@@ -14,100 +13,81 @@ public class Assignment1Part3 extends SuperKarel
      * Основна задача - це розмістити біпери так, як
      * розташовані чорні клітинки на шахівниці
      */
-    public void run() throws Exception
-    {
-        makeVerticalLine();
+    public void run() throws Exception {
+        //makeVerticalLine();
         makeOddRows();
         goToStartOfEvenRows();
         makeEvenRows();
     }
 
-    private void makeVerticalLine() throws Exception
-    {
-        putBeeper();
-        move();
-        if(frontIsClear()){
-            move();
-            makeVerticalLine();
-        }
+    private void makeVerticalLine() throws Exception {
 
     }
 
-    /**
-     * Передумови: Карел стоїть на початку першого непарного рядка, Дивиться на схід.
-     * Результат: Всі непарні рядки закінчені, Карел стоїть в північно-західному куті, дивлячись на схід.
-     */
-    private void makeOddRows() throws Exception
-    {
-        while (leftIsClear())
-        {
-            fillOddLine();
-            goBack();
-            carefulStepForward();
-            carefulStepForward();
+    private void makeOddRows() throws Exception{
+        makeOddRow();
+        goBack();
+        carefulStepForward();
+        if (frontIsClear()){
+            move();
             turnRight();
+            makeOddRows();
         }
-        if (leftIsBlocked())  //якщо висота непарна
-        {
-            fillOddLine();
-            goBack();
-        }
+    }
+
+
+    /**
+     * Передумови: Карел стоїть на початку непарного рядка, Дивиться на схід.
+     * Результат: Непарний рядок закінчені, Карел стоїть в кінці рядка, дивлячись на схід.
+     */
+    private void makeOddRow() throws Exception {
+        putBeeper();
+        carefulStepForward();
+        carefulStepForward();
+        if (frontIsClear()){
+            makeOddRow();
+        }//тут є проблема, якщо ширина непарна, то останній біпер він не кладе
     }
 
     /**
      * Передумови: Карел стоїть в кінці останнього непарного, дивиться на схід
      * Результат: Карел стоїть на початку першого парного, дивиться на схід
      */
-    private void goToStartOfEvenRows() throws Exception
-    {
-        turnRight();
-        moveToTheWall();
-        turnAround();
-        move();
-        turnRight();
+    private void goToStartOfEvenRows() throws Exception {
+
     }
 
     /**
      * Передумови: Карел стоїть на початку першого парного рядка, тобто на другому, дивиться на схід.
      * Результат: Всі парні рядки закінчені, Карел стоїть в кінці останнього, дивлячись на схід.
      */
-    private void makeEvenRows() throws Exception
-    {
+    private void makeEvenRows() throws Exception {
 
-        fillEvenLine();
-        goBack();
-        carefulStepForward();
-        carefulStepForward();
-        turnRight();
+
     }
 
     /**
      * Передумови: Карел стоїть на початку непарного ряду. Дивиться на схід.
      * Результат: Ряд закінчений, Карел стоїть в кінці непарного ряду, дивиться на схід.
      */
-    private void fillOddLine() throws Exception
-    {
-        while (frontIsClear())
-        {
+    private void fillOddLine() throws Exception {
+        while (frontIsClear()) {
             putBeeper();
             move();
-            if (frontIsClear())
-            {
+            if (frontIsClear()) {
                 move();
             }
         }
     }
 
-    private void fillEvenLine()
-    {
+    private void fillEvenLine() {
     }
 
     /**
      * Передумови: Карел стоїть в кінці закінченого рядку (будь якого), дивиться на схід.
      * Результат: Карел повернувся в початок ряду, дивиться на північ
      */
-    private void goBack() throws Exception
-    {
+    private void goBack() throws Exception {
         turnAround();
         moveToTheWall();
         turnRight();
@@ -117,11 +97,9 @@ public class Assignment1Part3 extends SuperKarel
      * Передумови: Карел стоїть на початку закінченого ряду. Дивиться на північ
      * Результат: Карел пройшов два кроки вперед, перевіряючи, чи немає стіни попереду. Дивиться на північ.
      */
-    private void carefulStepForward() throws Exception
-    {
+    private void carefulStepForward() throws Exception {
 
-        if (frontIsClear())
-        {
+        if (frontIsClear()) {
             move();
         }
 
