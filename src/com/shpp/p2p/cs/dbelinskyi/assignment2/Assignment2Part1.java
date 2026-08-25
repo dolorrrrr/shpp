1263353194ce572322bb2f00571a2e0bbe6ee38f
d.belinskyi <belinskijplay@gmail.com>
2026-08-25 15:40:53 +0300

    lection 3.1

--- src/com/shpp/p2p/cs/dbelinskyi/assignment2/Assignment2Part1.java
+++ src/com/shpp/p2p/cs/dbelinskyi/assignment2/Assignment2Part1.java
@@ -1,57 +1,32 @@
 package com.shpp.p2p.cs.dbelinskyi.assignment2;
 
-import acm.graphics.GRect;
+import acm.graphics.*;
 import com.shpp.cs.a.graphics.WindowProgram;
 
 import java.awt.*;
 
 public class Assignment2Part1 extends WindowProgram {
 
-    private static final int ITEM_SIZE= 40;
-    private static final int CIRCLES_COUNT_IN_COL = 8;
-    private static final int CIRCLES_COUNT_IN_ROW = 8;
-
     public void run() {
-        drawCheckerBoard(CIRCLES_COUNT_IN_COL, CIRCLES_COUNT_IN_ROW);
+        int r = 50;
+        int d = r * 2;
+        int x = getWidth() / 2 - r;
+        int y = getHeight() / 2 - (d * 3 / 2);
+        GOval c1 = createCircle(Color.RED, x, y, r);
+        add(c1);
+        GRectangle boundsOfKolo1 = c1.getBounds();
+        add(createCircle(Color.YELLOW, boundsOfKolo1.getX(), boundsOfKolo1.getY() + (r * 2), r));
+        add(createCircle(Color.GREEN, x, y + (r * 2) + (r * 2), r));
     }
 
-    private void drawCheckerBoard(int rows, int cols) {
-        boolean blackOrNot = true;
-        for (int i = 0; i < rows; i++){
-            drawCheckerBoardRow(i, cols, blackOrNot);
-            blackOrNot = reverseColor(blackOrNot);
-
-        }
+    private GOval createCircle(Color color, double x, double y, double r) {
+        GOval res = new GOval(x, y, r * 2, r * 2);
+        fillObject(res, color);
+        return res;
     }
 
-
-
-    private void drawCheckerBoardRow(int rowNumber, int cols, boolean blackOrNot) {
-        for (int i = 0; i < cols; i++){
-            drawCheckBoardItem(rowNumber, i, blackOrNot);
-            blackOrNot = reverseColor(blackOrNot);
-        }
-    }
-
-
-    private boolean reverseColor(boolean blackOrNot) {
-        if (blackOrNot){
-            return blackOrNot = false;
-
-        } else {
-            return blackOrNot = true;
-        }
-    }
-
-
-    private void drawCheckBoardItem(int rowNumber, int colNumber, boolean blackOrNot) {
-        GRect r = new GRect(
-                colNumber*ITEM_SIZE,
-                rowNumber*ITEM_SIZE,
-                ITEM_SIZE, ITEM_SIZE);
-        r.setFilled(true);
-        r.setFillColor(blackOrNot ? Color.BLACK : Color.WHITE);
-        r.setColor(Color.BLACK);
-        add(r);
+    private void fillObject(GFillable obj, Color color) {
+        obj.setFillColor(color);
+        obj.setFilled(true);
     }
 }
